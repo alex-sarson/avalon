fea668775eae7fe965eceb2c08c1ae3fe120d17f
notdospek <alex.luke.sarson@gmail.com>
2023-08-19 01:30:00 +0100

    slider & playerNames fields

--- screens/SetupScreen.js
+++ screens/SetupScreen.js
@@ -1,5 +1,14 @@
-import { Text, SafeAreaView, StyleSheet, Button } from 'react-native';
-import { useForm, Controller } from 'react-hook-form';
+import {
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  Button,
+  ScrollView,
+  View,
+} from 'react-native';
+import { useState } from 'react';
+import Slider from '@react-native-community/slider';
+import PlayerNames from '../components/PlayerNames';
 
 // Setup breakdown: https://www.ultraboardgames.com/avalon/game-rules.php
 // Number of players
@@ -15,10 +24,36 @@ import { useForm, Controller } from 'react-hook-form';
 // Morgana check
 
 export default function SetupScreen() {
+  const [settings, setSettings] = useState({
+    numberOfPlayers: 5,
+  });
+
+  const [playerData, setPlayerData] = useState([]);
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>This is the setup screen</Text>
-      <Button title="Start Game" onPress={() => console.log('start game')} />
+      <ScrollView>
+        <View style={styles.numberOfPlayers}>
+          <Text>Choose how many players are in this game</Text>
+          <Slider
+            style={{ width: 200, height: 40 }}
+            step={1}
+            minimumValue={5}
+            maximumValue={10}
+            value={settings.numberOfPlayers}
+            onValueChange={(value) =>
+              setSettings({ ...settings, numberOfPlayers: value })
+            }
+          />
+          <Text>{settings.numberOfPlayers}</Text>
+        </View>
+        <PlayerNames
+          count={settings.numberOfPlayers}
+          playerData={playerData}
+          setPlayerData={setPlayerData}
+        />
+      </ScrollView>
+      <Button title="Start Game" onPress={() => console.log(playerData)} />
     </SafeAreaView>
   );
 }
@@ -26,8 +61,15 @@ export default function SetupScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'pink',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  numberOfPlayers: {
+    padding: 20,
+    alignItems: 'center',
+    borderColor: 'black',
+    borderWidth: 1,
   },
 });
