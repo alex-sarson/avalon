dd9103c6c95355348f05e9ee7b13ac55c8001fac
notdospek <alex.luke.sarson@gmail.com>
2023-08-21 00:02:20 +0100

    setups nearly finalised - still messy

--- screens/SetupScreen.js
+++ screens/SetupScreen.js
@@ -1,14 +1,15 @@
 import {
   Text,
+  TextInput,
   SafeAreaView,
   StyleSheet,
   Button,
   ScrollView,
   View,
 } from 'react-native';
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 import Slider from '@react-native-community/slider';
-import PlayerNames from '../components/PlayerNames';
+import CheckBox from 'expo-checkbox';
 
 // Setup breakdown: https://www.ultraboardgames.com/avalon/game-rules.php
 // Number of players
@@ -27,8 +28,27 @@ export default function SetupScreen() {
   const [settings, setSettings] = useState({
     numberOfPlayers: 5,
   });
+  const [playerNames, setPlayerNames] = useState([]);
+  const [additionalChars, setAdditionalChars] = useState(false);
+  const [nameErrors, setNameErrors] = useState({});
 
-  const [playerData, setPlayerData] = useState([]);
+  const handleSliderChange = (value) => {
+    setSettings({ ...settings, numberOfPlayers: value });
+    setPlayerNames((prev) => {
+      return [...prev.slice(0, settings.numberOfPlayers)];
+    });
+    setNameErrors((prev) => {
+      const errors = { ...prev };
+      // filter out errors for non-existing fields
+      Object.keys(errors)
+        .filter((key) => Number(key) >= value)
+        .forEach((key) => delete errors[key]);
+
+      return errors;
+    });
+  };
+
+  const fields = Array.from({ length: settings.numberOfPlayers }, (_, i) => i);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -41,19 +61,63 @@ export default function SetupScreen() {
             minimumValue={5}
             maximumValue={10}
             value={settings.numberOfPlayers}
-            onValueChange={(value) =>
-              setSettings({ ...settings, numberOfPlayers: value })
-            }
+            onValueChange={handleSliderChange}
           />
           <Text>{settings.numberOfPlayers}</Text>
         </View>
-        <PlayerNames
-          count={settings.numberOfPlayers}
-          playerData={playerData}
-          setPlayerData={setPlayerData}
-        />
+
+        <View style={styles.playerNames}>
+          {fields.map((i) => (
+            <Fragment key={`field${i}`}>
+              <TextInput
+                key={i}
+                style={styles.text}
+                placeholder={'Player ' + (i + 1)}
+                onBlur={() => {
+                  if (!playerNames[i]) {
+                    setNameErrors({ ...nameErrors, [i]: 'Name is required' });
+                  }
+                }}
+                onChangeText={(text) => {
+                  const errors = { ...nameErrors };
+                  delete errors[i];
+                  setNameErrors(errors);
+
+                  const values = [...playerNames];
+                  values[i] = text;
+                  setPlayerNames(values);
+                }}
+              />
+              {nameErrors[i] && (
+                <Text style={styles.error} key={`error ${i}`}>
+                  {nameErrors[i]}
+                </Text>
+              )}
+            </Fragment>
+          ))}
+        </View>
+
+        <View style={styles.additionalChars}>
+          <Text>Additional characters</Text>
+          <CheckBox
+            disabled={false}
+            value={additionalChars}
+            onValueChange={(value) => setAdditionalChars(value)}
+            style={styles.checkBox}
+          />
+        </View>
       </ScrollView>
-      <Button title="Start Game" onPress={() => console.log(playerData)} />
+      <Button
+        title="Start Game"
+        onPress={() => {
+          console.log(playerNames, additionalChars, settings.numberOfPlayers);
+        }}
+        disabled={
+          playerNames.length < 5 ||
+          playerNames.length != settings.numberOfPlayers
+          // Object.keys(nameErrors) && Object.keys()
+        }
+      />
     </SafeAreaView>
   );
 }
@@ -71,5 +135,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderColor: 'black',
     borderWidth: 1,
+  },
+
+  additionalChars: {
+    marginTop: 20,
+    flexDirection: 'row',
+  },
+
+  text: {
+    borderWidth: 1,
+    borderColor: 'black',
+    backgroundColor: 'lightgrey',
+    padding: 4,
+    marginTop: 20,
+  },
+
+  error: {
+    color: 'red',
+  },
+
+  checkBox: {
+    marginLeft: 10,
   },
 });
